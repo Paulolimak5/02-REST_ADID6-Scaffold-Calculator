bf5fcaad9d3128644e9d1b4bae764da066b9b401
Paulo <p.manoel.m@gmail.com>
2021-04-12 23:11:55 -0300

    New Commit

---
+++ src/main/java/br/edu/ifsp/exception/ExceptionResponse.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.exception;
+
+import java.io.Serializable;
+
+public class ExceptionResponse implements Serializable{
+
+}
---
+++ src/main/java/br/edu/ifsp/exception/UnsuportedMathOperationResponse.java
@@ -0,0 +1,5 @@
+package br.edu.ifsp.exception;
+
+public class UnsuportedMathOperationResponse {
+
+}
